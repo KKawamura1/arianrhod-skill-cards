35cb3f18968bd00f8ae89ddfeaf3836f4a779b43
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2019-03-22 18:48:03 +0900

    update skill_range_from_text

--- skill_cards_generator/skill_range.py
+++ skill_cards_generator/skill_range.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import enum
 from dataclasses import dataclass
+import re
 
 
 class RangeUnit(enum.Enum):
@@ -21,25 +22,55 @@ class SkillRangeKind(enum.Enum):
     sight = enum.auto()
     scene = enum.auto()
     with_unit = enum.auto()
+    string = enum.auto()
 
 
 @dataclass
 class SkillRange:
+    kind_table = {
+        '武器': SkillRangeKind.weapon,
+        '視界': SkillRangeKind.sight,
+        'シーン': SkillRangeKind.scene,
+    }
+    metre = re.compile(r'^\s*([0-9]+)\s*m$', re.IGNORECASE)
+    square = re.compile(r'^\s*([0-9]+)\s*sq$', re.IGNORECASE)
+
     def __init__(
         self,
         kind: SkillRangeKind,
         value: int = None,
-        unit: RangeUnit = RangeUnit.m
+        unit: RangeUnit = RangeUnit.m,
+        string: str = None
     ) -> None:
         if kind is SkillRangeKind.with_unit:
             assert value is not None
+        elif kind is SkillRangeKind.string:
+            assert string is not None
         self._kind = kind
         self._value = value
         self._unit = unit
+        self._string = string
 
     @staticmethod
     def from_text(text: str) -> SkillRange:
-        return SkillRange(SkillRangeKind.with_unit, 0)
+        if text in SkillRange.kind_table:
+            return SkillRange(SkillRange.kind_table[text])
+        if text == '至近':
+            return SkillRange(SkillRangeKind.with_unit, 0)
+        if SkillRange.metre.match(text) is not None:
+            return SkillRange(
+                SkillRangeKind.with_unit,
+                int(SkillRange.metre.match(text).group(1)),
+                RangeUnit.m
+            )
+        if SkillRange.square.match(text) is not None:
+            return SkillRange(
+                SkillRangeKind.with_unit,
+                int(SkillRange.square.match(text).group(1)),
+                RangeUnit.sq
+            )
+
+        return SkillRange(SkillRangeKind.string, string=text)
 
     def __str__(self):
         if self._kind is SkillRangeKind.nothing:
@@ -50,9 +81,12 @@ class SkillRange:
             return '視界'
         elif self._kind is SkillRangeKind.scene:
             return 'シーン'
-        else:
-            assert (self._kind is SkillRangeKind.with_unit
-                    and self._value is not None)
+        elif self._kind is SkillRangeKind.with_unit:
+            assert self._value is not None
             if self._unit == RangeUnit.m and self._value == 0:
                 return '至近'
             return str(self._value) + str(self._unit)
+        else:
+            assert self._kind is SkillRangeKind.string
+            assert self._string is not None
+            return self._string
